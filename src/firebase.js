723abb8ec9a37fa7867e9f778867f7c48aafec9c
Anastasia <80560435+Stasies@users.noreply.github.com>
2022-09-12 14:20:54 +0500

    security

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCeIQsNbomTnaynsp_STn2anj71JuWHcqE",
-  authDomain: "chat-2b2ce.firebaseapp.com",
-  projectId: "chat-2b2ce",
-  storageBucket: "chat-2b2ce.appspot.com",
-  messagingSenderId: "850103016373",
-  appId: "1:850103016373:web:37cc42e405514adda4d7fc",
-  measurementId: "G-VVKF1Y49FZ",
-  storageBucket: "chat-2b2ce.appspot.com",
+  //
 };
 
 // Initialize Firebase
